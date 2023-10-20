5c364d91f507a8931d0d0ec97c3c2d148db8b953
พัชรพล แก้วประสิทธิ์ <phatcharaphonpoom2546@gmail.com>
2023-10-21 01:41:41 +0700

    function for edit & delete tag

--- psunote/noteapp.py
+++ psunote/noteapp.py
@@ -3,6 +3,9 @@ import flask
 import models
 import forms
 
+from flask import render_template, request, redirect, url_for, flash
+from forms import NoteForm, TagForm
+from models import Tag, db, Note
 
 app = flask.Flask(__name__)
 app.config["SECRET_KEY"] = "This is secret key"
@@ -76,6 +79,30 @@ def tags_view(tag_name):
         notes=notes,
     )
 
+@app.route("/notes/edit/<int:note_id>", methods=["GET", "POST"])
+def notes_edit(note_id):
+    db = models.db
+    note = db.session.query(models.Note).get(note_id)
+    if not note:
+        return "Note not found", 404
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    form = forms.NoteForm(obj=note)
+    if form.validate_on_submit():
+        form.populate_obj(note)
+        db.session.commit()
+        return flask.redirect(flask.url_for("index"))
+
+    return flask.render_template("notes-edit.html", form=form, note=note)
+@app.route("/notes/delete/<int:note_id>", methods=["GET", "POST"])
+def notes_delete(note_id):
+    note = Note.query.get(note_id)
+    if not note:
+        return "Note not found", 404
+
+    form = NoteForm()
+    if request.method == "POST":
+        db.session.delete(note)
+        db.session.commit()
+        return redirect(url_for("index"))
+
+    return render_template("notes-del.html", note=note, form=form)
